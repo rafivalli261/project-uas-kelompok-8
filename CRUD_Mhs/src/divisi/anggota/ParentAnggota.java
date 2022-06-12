23f7ebb6e9b812dbf785b36817d97bf58a72cd96
rafivalli261 <valliansyah@gmail.com>
2022-06-12 21:42:45 +0700

    menambahkan kelas untuk inheritance

---
+++ CRUD_Mhs/src/divisi/anggota/ParentAnggota.java
@@ -0,0 +1,18 @@
+package divisi.anggota;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Kelompok 8
+ */
+public class ParentAnggota extends JFrame {
+
+    public void inisiasi() {
+        System.out.println("Ini method untuk menginisiasi awal dari tabel");
+    }
+
+    public void tampilkan() {
+        System.out.println("Ini method untuk menampilkan tabel");
+    }
+}
